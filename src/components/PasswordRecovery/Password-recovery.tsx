78d884a67ccbad8c8d0d68fcac4571b12fff4af8
espere-papillon <marinabov007@gmail.com>
2021-06-27 23:53:13 +0300

    add styles for password recovery page

--- src/components/PasswordRecovery/Password-recovery.tsx
+++ src/components/PasswordRecovery/Password-recovery.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styles from './Password-recovery.module.scss'
+import {NavLink} from "react-router-dom";
 
 export const PasswordRecovery = () => {
     return (
-        <div>PasswordRecovery</div>
+        <div className={styles.container}>
+            <div><h1 className={styles.header}>It-incubator</h1></div>
+            <h2 className={styles.header}>Forgot your password?</h2>
+            <input type="email" placeholder={'Email'}/>
+            <p className={styles.textAction}>Enter your email address and we will send you further instructions</p>
+            <button>Send Instructions</button>
+            <p className={styles.textRememberPassword}>Did you remember your password?</p>
+            <NavLink to="/login" activeClassName={styles.login}>Try logging in</NavLink>
+        </div>
     )
 }
