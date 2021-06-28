27c749169ad113cd07432365a1f31fe7673510f9
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-06-28 20:12:54 +0300

    I created a new branch, rendered jsx

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React from 'react';
+import s from './Registration.module.scss'
 
 export const Registration = () => {
     return (
-        <div>Registration</div>
+        <div className={s.registrationContainer}>
+            <div className={s.logo}>It-incubator</div>
+            <h1>Sign Up</h1>
+            <div className={s.inputFields}>
+                <label>
+                    <span className={s.inputName}>Email</span>
+                    <input/>
+                </label>
+                <label>
+                    <span className={s.inputName}>Password</span>
+                    <input type={'password'}/>
+                    <a href='#' className={s.passwordControl}/>
+                </label>
+                <label>
+                    <span className={s.inputName}>Confirm password</span>
+                    <input type={'password'}/>
+                </label>
+            </div>
+            <div className={s.btn}>
+                <a className={s.btnCancel}>Cancel</a>
+                <a className={s.btnRegister}>Register</a>
+            </div>
+        </div>
     )
 }
