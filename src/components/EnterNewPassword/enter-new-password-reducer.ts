d8b54eb9b40ed13dd125637f0bcff6453c08816e
espere-papillon <marinabov007@gmail.com>
2021-06-29 00:26:29 +0300

    add BLL for set new password, reducer, thunk, action

--- src/components/EnterNewPassword/enter-new-password-reducer.ts
+++ src/components/EnterNewPassword/enter-new-password-reducer.ts
@@ -1,20 +1,68 @@
-const initialStateNewPassword = {
+import { AxiosError } from "axios";
+import {AppThunkType} from "../../state/redux-store";
+import {SetNewPasswordAPI} from "./enter-new-password-api";
+import {PasswordRecoveryAPI} from "../PasswordRecovery/password-recovery-api";
 
+const initialStateSetNewPassword = {
+    error: '',
+    loading: false,
+    success: false,
 }
 
-export const enterNewPasswordReducer = (state: initialStateNewPasswordType = initialStateNewPassword, action: actionsNewPasswordType) => {
+//types
+export type initialSetNewPasswordType = typeof initialStateSetNewPassword
+
+const setError = (error: string) => ({type: "set-new-password/SET_ERROR", error} as const)
+const setLoading = (loading: boolean) => ({type: "set-new-password/SET_LOADING", loading} as const)
+const setSuccess = (success: boolean) => ({type: "set-new-password/SET_SUCCESS", success} as const)
+
+export type actionsSetNewPasswordType = ReturnType<typeof setError> | ReturnType<typeof setLoading> | ReturnType<typeof setSuccess>
+
+export const setNewPasswordReducer = (state: initialSetNewPasswordType = initialStateSetNewPassword, action: actionsSetNewPasswordType): initialSetNewPasswordType => {
     switch (action.type) {
-        default:
+        case "set-new-password/SET_ERROR": {
+            return {
+                ...state,
+                error: action.error,
+                loading: false,
+                success: false,
+            }
+        }
+        case "set-new-password/SET_LOADING": {
+            return {
+                ...state,
+                error: "",
+                loading: action.loading,
+                success: false,
+            }
+        }
+        case "set-new-password/SET_SUCCESS": {
+            return {
+                ...state,
+                error: "",
+                loading: false,
+                success: action.success,
+            }
+        }
+        default: {
             return state
+        }
     }
 }
 
-
-//types
-export type initialStateNewPasswordType = {
-
-}
-export type actionsNewPasswordType = any
-
-
+export const setNewPasswordThunk = (password: string, token: string): AppThunkType => async (
+    dispatch) => {
+    dispatch(setLoading(true));
+    SetNewPasswordAPI.setNewPassword(password, token)
+        .then(res => {
+            if (!res.error) {
+                console.log(res.error)
+            } else {
+                console.log("Not errors")
+            }
+        })
+        .catch((error: AxiosError) => {
+            console.log(error)
+        })
+};
 
