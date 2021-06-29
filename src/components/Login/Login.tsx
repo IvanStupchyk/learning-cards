315434f0438b3294a056aa5cd0d97997bf8bb9f3
IvanStupchuk <hardtrynew@rambler.ru>
2021-06-29 21:27:24 +0300

    added email and password validation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -16,18 +16,29 @@ export const Login = () => {
     const errorMessage = useSelector<AppStateType, null | string>(state => state.login.error)
 
     const emailTest = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(emailValue)
+    const passwordTest = /[0-9a-zA-Z!@#$%^&*]{6,}/.test(passwordValue)
+    const [errorEmailMessage, setErrorEmailMessage] = useState<string>('')
+    const [errorPasswordMessage, setErrorPasswordMessage] = useState<string>('')
 
     const changeEmailValue = (e: ChangeEvent<HTMLInputElement>) => {
         setEmailValue(e.currentTarget.value)
+        setErrorEmailMessage('')
         errorMessage && dispatch(incorrectDataLogIn(''))
     }
     const changePasswordValue = (e: ChangeEvent<HTMLInputElement>) => {
         setPasswordValue(e.currentTarget.value)
         errorMessage && dispatch(incorrectDataLogIn(''))
+        setErrorPasswordMessage('')
     }
 
     const checkLoginUser = () => {
-        dispatch(loginUserTC(emailValue, passwordValue))
+        if (!emailTest) {
+            setErrorEmailMessage('Incorrect email')
+        } else if (!passwordTest) {
+            setErrorPasswordMessage('Minimum 6 characters')
+        } else {
+            dispatch(loginUserTC(emailValue, passwordValue))
+        }
     }
 
     if (isLogIn) {
@@ -44,10 +55,12 @@ export const Login = () => {
                 <label>
                     <span className={st.titleInput}>Email</span>
                     <input value={emailValue} onChange={changeEmailValue}/>
+                    <span className={st.errorEmailPasswordMessage}>{errorEmailMessage}</span>
                 </label>
                 <label>
                     <span className={st.titleInput}>Password</span>
                     <input type={'password'} value={passwordValue} onChange={changePasswordValue}/>
+                    <span className={st.errorEmailPasswordMessage}>{errorPasswordMessage}</span>
                 </label>
                 <div className={st.forgotPasswordBtn}>
                     <a onClick={() => {
