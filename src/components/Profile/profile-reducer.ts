28fb8602300dd7e6f0c71a209820b24c87b743e9
zhirkovpetr <zhirkovpetr95@gmail.com>
2021-07-05 00:14:19 +0300

    update profile-reducer

--- src/components/Profile/profile-reducer.ts
+++ src/components/Profile/profile-reducer.ts
@@ -1,17 +1,35 @@
 const initialStateProfile = {
-
+   profile:{
+       _id: '',
+       email: '',
+       name: '',
+       avatar: '',
+       publicCardPacksCount: 0
+   }
 }
 
 export const profileReducer = (state: initialProfileType = initialStateProfile, action: actionsProfileType) => {
     switch (action.type) {
+        case 'PROFILE/SET-PROFILE':
+            return {...state, ...action.payload}
+        case 'PROFILE/UPDATE-PROFILE':
+            return {...state, profile: state.profile, avatar: action.payload.avatar, name: action.payload.name}
         default:
             return state
     }
 }
 
+//actionC
+export const setProfileAC = (profile: initialProfileType) => ({
+    type: 'PROFILE/SET-PROFILE', payload: {profile}
+} as const)
+const updateProfileAC = (avatar: string, name: string) => ({
+    type: 'PROFILE/UPDATE-PROFILE',
+    payload: {avatar, name}
+} as const)
+
 
 //types
-export type initialProfileType = {
-
-}
-export type actionsProfileType = any
+export type initialProfileType = typeof initialStateProfile
+export type actionsProfileType = | ReturnType<typeof setProfileAC>
+    | ReturnType<typeof updateProfileAC>
