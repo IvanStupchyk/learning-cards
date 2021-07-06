e2c1e2a037b1ffe35e8a569356a686639f1725cf
espere-papillon <marinabov007@gmail.com>
2021-07-07 01:24:13 +0300

    update function add for packs and cards

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -1,16 +1,19 @@
 import s from './CardsList.module.scss'
 import {useDispatch, useSelector} from "react-redux";
-import React, {useEffect} from "react";
-import {getCardsList} from "./cardsList-reducer";
+import React, {ChangeEvent, useEffect, useState} from "react";
+import {addCard, getCardsList} from "./cardsList-reducer";
 import {AppStateType} from "../../state/redux-store";
 import {cardType} from "../../api/api";
 import {Redirect, useParams} from "react-router-dom";
 import {AuthUser} from "../Login/login-reducer";
 import {Preloader} from "../../common/Preloader/Preloader";
+import {addPack} from "../PacksList/packsList-reducer";
 
 export const CardsList = () => {
     const isAuth = useSelector<AppStateType, boolean>(state => state.login.logIn)
     const idUser = useSelector<AppStateType, string>(state => state.login._id)
+    const [title, setTitle] = useState<string>("")
+    const [error, setError] = useState<string | null>(null)
     const dispatch = useDispatch();
     const {id} = useParams<{ id: string }>()
 
@@ -20,17 +23,32 @@ export const CardsList = () => {
         } else {
             dispatch(getCardsList({cardPack_id: id}))
         }
-    }, [dispatch])
+    }, [dispatch, id])
 
     const cardsList = useSelector<AppStateType, Array<cardType>>(state => state.cardsList)
+
+    const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        error && setError(null)
+        setTitle(e.currentTarget.value)
+    }
+
+    const addCardFun = () => {
+        const trimmedTitle = title.trim()
+        if (trimmedTitle) {
+            dispatch(addCard({card: {cardsPack_id: id}}))
+        } else {
+            setError("Title is required")
+        }
+        setTitle("")
+    }
 
     if (!isAuth) {
         return <Redirect to={'/login'}/>
     }
 
-    if (!cardsList.length) {
-        return <Preloader/>
-    }
+    // if (!cardsList.length) {
+    //     return <Preloader/>
+    // }
 
     return (
         <table className={s.table}>
@@ -42,8 +60,10 @@ export const CardsList = () => {
                 <th className={s.tableHeader}>{"ANSWER"}</th>
                 <th className={s.tableHeader}>{"GRADE"}</th>
                 <th className={s.tableHeader}>{"UPDATED"}</th>
-                <th><input type={'text'}/></th>
-                <th><button>ADD</button></th>
+                <th><input type={'text'} onChange={changeTitle}/></th>
+                <th>
+                    <button onClick={addCardFun}>ADD</button>
+                </th>
             </tr>
             {cardsList.map((card) => (
                 <tr key={card._id} className={s.tableRow}>
@@ -54,8 +74,12 @@ export const CardsList = () => {
                     <td className={s.tableCol}>{card.answer}</td>
                     <td className={s.tableCol}>{card.grade}</td>
                     <td className={s.tableCol}>{card.updated}</td>
-                    <td><button>DELETE</button></td>
-                    <td><button>UPDATE</button></td>
+                    <td>
+                        <button>DELETE</button>
+                    </td>
+                    <td>
+                        <button>UPDATE</button>
+                    </td>
                 </tr>
             ))}
         </table>
--- src/components/PacksList/PacksList.tsx
+++ src/components/PacksList/PacksList.tsx
@@ -1,7 +1,7 @@
 import s from './PacksList.module.scss'
 import {useDispatch, useSelector} from "react-redux";
-import React, {useEffect} from "react";
-import {getPackList} from "./packsList-reducer";
+import React, {ChangeEvent, useEffect, useState} from "react";
+import {addPack, getPackList} from "./packsList-reducer";
 import {AppStateType} from "../../state/redux-store";
 import {cardsPackType} from "../../api/api";
 import {NavLink, Redirect} from "react-router-dom";
@@ -11,6 +11,9 @@ import {Preloader} from "../../common/Preloader/Preloader";
 export const PacksList = () => {
     const isAuth = useSelector<AppStateType, boolean>(state => state.login.logIn)
     const idUser = useSelector<AppStateType, string>(state => state.login._id)
+    const [checkedPrivate, setCheckedPrivate] = useState<boolean>(false)
+    const [title, setTitle] = useState<string>("")
+    const [error, setError] = useState<string | null>(null)
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -23,6 +26,30 @@ export const PacksList = () => {
 
     const packsList = useSelector<AppStateType, Array<cardsPackType>>(state => state.packsList)
 
+    const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        error && setError(null)
+        setTitle(e.currentTarget.value)
+    }
+
+    const addPackFun = () => {
+        const trimmedTitle = title.trim()
+        if (trimmedTitle) {
+            dispatch(addPack({cardsPack: {name: title, private: checkedPrivate}}))
+        } else {
+            setError("Title is required")
+        }
+        setTitle("")
+    }
+
+    const getPrivatePacks = () => {
+        setCheckedPrivate(!checkedPrivate)
+        if(!checkedPrivate) {
+            dispatch(getPackList({user_id: idUser}))
+        } else {
+            dispatch(getPackList({}))
+        }
+    }
+
     if (!isAuth) {
         return <Redirect to={'/login'}/>
     }
@@ -32,32 +59,45 @@ export const PacksList = () => {
     }
 
     return (
-        <table className={s.table}>
-            <tr className={s.tableRow}>
-                {/*{Object.keys(packsList[0]).map((key) => (*/}
-                {/*    <th>{key}</th>*/}
-                {/*))}*/}
-                <th className={s.tableHeader}>{"NAME"}</th>
-                <th className={s.tableHeader}>{"CARDS COUNT"}</th>
-                <th className={s.tableHeader}>{"USER NAME"}</th>
-                <th className={s.tableHeader}>{"UPDATED"}</th>
-                <th><input type={'text'}/></th>
-                <th><button>ADD</button></th>
-            </tr>
-            {packsList.map((pack) => (
-                <tr key={pack._id} className={s.tableRow}>
-                    {/*{Object.values(pack).map((val) => (*/}
-                    {/*    <td style={{border: '1px solid black'}}>{val}</td>*/}
+        <>
+            <div>
+                <input type="checkbox" className="toggle_input" onChange={getPrivatePacks}/>
+                <label>private</label>
+            </div>
+            <table className={s.table}>
+                <tr className={s.tableRow}>
+                    {/*{Object.keys(packsList[0]).map((key) => (*/}
+                    {/*    <th>{key}</th>*/}
                     {/*))}*/}
-                    <td className={s.tableCol}>{pack.name}</td>
-                    <td className={s.tableCol}>{pack.cardsCount}</td>
-                    <td className={s.tableCol}>{pack.user_name}</td>
-                    <td className={s.tableCol}>{pack.updated}</td>
-                    <td><button>DELETE</button></td>
-                    <td><button>UPDATE</button></td>
-                    <td><NavLink to={`/cards-list/${pack._id}`} activeClassName={s.activeLink}>cards list</NavLink></td>
+                    <th className={s.tableHeader}>{"NAME"}</th>
+                    <th className={s.tableHeader}>{"CARDS COUNT"}</th>
+                    <th className={s.tableHeader}>{"USER NAME"}</th>
+                    <th className={s.tableHeader}>{"UPDATED"}</th>
+                    <th><input type={'text'} onChange={changeTitle}/></th>
+                    <th>
+                        <button onClick={addPackFun}>ADD</button>
+                    </th>
                 </tr>
-            ))}
-        </table>
+                {packsList.map((pack) => (
+                    <tr key={pack._id} className={s.tableRow}>
+                        {/*{Object.values(pack).map((val) => (*/}
+                        {/*    <td style={{border: '1px solid black'}}>{val}</td>*/}
+                        {/*))}*/}
+                        <td className={s.tableCol}>{pack.name}</td>
+                        <td className={s.tableCol}>{pack.cardsCount}</td>
+                        <td className={s.tableCol}>{pack.user_name}</td>
+                        <td className={s.tableCol}>{pack.updated}</td>
+                        <td>
+                            <button>DELETE</button>
+                        </td>
+                        <td>
+                            <button>UPDATE</button>
+                        </td>
+                        <td><NavLink to={`/cards-list/${pack._id}`} activeClassName={s.activeLink}>cards list</NavLink>
+                        </td>
+                    </tr>
+                ))}
+            </table>
+        </>
     )
 }
