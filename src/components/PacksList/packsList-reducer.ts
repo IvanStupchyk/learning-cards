2ac19cd2259028c1912ec91dc9e609c0d0892751
espere-papillon <marinabov007@gmail.com>
2021-07-08 21:30:51 +0300

    add thunk delete pack

--- src/components/PacksList/packsList-reducer.ts
+++ src/components/PacksList/packsList-reducer.ts
@@ -7,22 +7,31 @@ import {
 import {AppThunkType, GetAppStateType} from "../../state/redux-store";
 import {Dispatch} from "redux";
 
-const initialState: Array<cardsPackType> = []
+const initialState = {
+    arrayPack: [] as Array<cardsPackType>,
+    success: false
+}
+
+type initialStateType = typeof initialState
 
 //types
 type GetPacksListAT = ReturnType<typeof GetPacksListAC>
+type SetSuccessAT = ReturnType<typeof SetSuccessAC>
 // type AddPackAT = ReturnType<typeof AddPackAC>
 
-export type actionPacksListType = GetPacksListAT
+export type actionPacksListType = GetPacksListAT | SetSuccessAT
 
 //actionC
 export const GetPacksListAC = (params: Array<cardsPackType>) => ({type: 'packList/GET-PACKSLIST', params} as const)
+export const SetSuccessAC = (success: boolean) => ({type: 'packList/SET-SUCCESS', success} as const)
 //export const AddPackAC = (payload: addCardsPackDataType) => ({type: "packList/ADD-PACK", payload} as const)
 
-export const packsListReducer = (state = initialState, action: actionPacksListType): Array<cardsPackType> => {
+export const packsListReducer = (state = initialState, action: actionPacksListType): initialStateType => {
     switch (action.type) {
         case "packList/GET-PACKSLIST":
-            return action.params.map(pl => ({...pl}))
+            return {...state, arrayPack: action.params.map(pl => ({...pl}))}
+        case "packList/SET-SUCCESS":
+            return {...state, success: action.success}
         // case "packList/ADD-PACK":
         //     return [...state, {...action.payload}]
         default:
@@ -36,10 +45,12 @@ export const getPackList = (params: getPacksAPIParamsType): AppThunkType => asyn
     try {
         const response = await PacksListAPI.getPacks({...params})
         dispatch(GetPacksListAC(response.data.cardPacks))
+        dispatch(SetSuccessAC(true))
     } catch (e) {
         const error = e.response
             ? e.response.data.error
             : (e.message + ', more details in the console');
+        dispatch(SetSuccessAC(false))
     } finally {
     }
 }
@@ -48,6 +59,20 @@ export const addPack = (data: addCardsPackDataType): AppThunkType => async (disp
 
     try {
         const responseAdd = await PacksListAPI.addCardsPack(data)
+        const response = await PacksListAPI.getPacks({})
+        dispatch(GetPacksListAC(response.data.cardPacks))
+    } catch (e) {
+        const error = e.response
+            ? e.response.data.error
+            : (e.message + ', more details in the console');
+    } finally {
+    }
+}
+
+export const deletePack = (params: {id: string}): AppThunkType => async (dispatch: Dispatch<actionPacksListType>) => {
+
+    try {
+        const responseDelete = await PacksListAPI.deleteCardsPack(params)
         const response = await PacksListAPI.getPacks({})
         dispatch(GetPacksListAC(response.data.cardPacks))
     } catch (e) {
