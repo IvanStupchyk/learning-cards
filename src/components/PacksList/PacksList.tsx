7d396b3f9cb2ee3b8daa9d0bbb9fd054825c63f9
espere-papillon <marinabov007@gmail.com>
2021-07-12 23:19:01 +0300

    add css for modal window

--- src/components/PacksList/PacksList.tsx
+++ src/components/PacksList/PacksList.tsx
@@ -9,8 +9,9 @@ import {AuthUser} from "../Login/login-reducer";
 import {Preloader} from "../../common/Preloader/Preloader";
 import {Pagination} from "../../common/Pagination/Pagination";
 import {ManagePacksButton} from './ManagePacksButton';
+import {InputContainer} from "../../common/InputContainer/InputContainer";
 
-export const PacksList = () => {
+export const PacksList = (props: {user_id?: string}) => {
     const isAuth = useSelector<AppStateType, boolean>(state => state.login.logIn)
     const idUser = useSelector<AppStateType, string>(state => state.profile.profile._id)
     const success = useSelector<AppStateType, boolean>(state => state.packsList.success)
@@ -22,7 +23,7 @@ export const PacksList = () => {
     const dispatch = useDispatch();
 
     const {
-        page = 1, pageCount = 10, min = 1, max = 10, packName, sortPacks
+        page = 1, pageCount = 10, min = 0, max = 10, packName, sortPacks
     } = useSelector<AppStateType, getPacksAPIParamsType>(state => state.packsList.packsParams);
 
     const cardPacksTotalCount = useSelector<AppStateType, number>(state => state.packsList.cardPacksTotalCount);
@@ -37,18 +38,17 @@ export const PacksList = () => {
         if (!idUser) {
             dispatch(AuthUser())
         } else {
-            dispatch(getPackList({pageCount, min, max, page, packName}))
+            getPrivatePacks()
         }
     }, [dispatch, page, pageCount, sortPacks, min, max, packName])
 
 
     const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
         error && setError(null)
-        setTitle(e.currentTarget.value)
+        setTitle(e.target.value)
     }
 
     const addPackFun = () => {
-        console.log(1)
         const trimmedTitle = title.trim()
         if (trimmedTitle) {
             setCheckedPrivate(false)
@@ -69,7 +69,7 @@ export const PacksList = () => {
         const trimmedSearch = packNameTitle.trim()
         if (trimmedSearch) {
             dispatch(setPackNameAC(trimmedSearch))
-            // getPrivatePacks() ARBUZ <3
+            getPrivatePacks()
         } else {
             setError("Title is required")
         }
@@ -81,10 +81,8 @@ export const PacksList = () => {
     }
 
     const getPrivatePacks = () => {
-        setCheckedPrivate(prev => !prev)
-        if (!checkedPrivate) {
-            debugger
-            dispatch(getPackList({pageCount, min, max, page, packName, user_id: idUser}))
+        if (props.user_id) {
+            dispatch(getPackList({pageCount, min, max, page, packName, user_id: props.user_id}))
         } else {
             dispatch(getPackList({pageCount, min, max, page, packName}))
         }
@@ -104,14 +102,21 @@ export const PacksList = () => {
     return (
         <>
             <div className={s.flex}>
-                <div className={s.private}>
+                {props.user_id && <div className={s.private}>
                     <input type="checkbox" className="toggle_input" onChange={getPrivatePacks}
                            checked={checkedPrivate}/>
                     <label>private</label>
-                </div>
+                </div>}
                 <div className={s.search}>
                     <div className={s.containerInputSearch}>
-                        <input type={'text'} onChange={changeSearch} value={packNameTitle}/>
+                        <InputContainer
+                            placeholder={'Search'}
+                            changeValue={changeSearch}
+                            errorMessage={''}
+                            typeInput={'text'}
+                            value={packNameTitle}
+                        />
+                        {/*<input type={'text'} onChange={changeSearch} value={packNameTitle}/>*/}
                         <button onClick={() => {
                             dispatch(setPackNameAC(''))
                         }}>X
@@ -126,9 +131,9 @@ export const PacksList = () => {
                         <th className={s.tableHeader}>{"USER NAME"}</th>
                         <th className={s.tableHeader}>{"UPDATED"}</th>
                         <th>
-                            <input type={'text'} value={title} onChange={changeTitle} />
-                            <button onClick={addPackFun}>ADD</button>
-                            {/*<button onClick={() => setShowModal(true)}>ADD</button>*/}
+                            {/*<input type={'text'} value={title} onChange={changeTitle} />*/}
+                            {/*<button onClick={addPackFun}>ADD</button>*/}
+                            <button onClick={() => setShowModal(true)}>ADD</button>
                         </th>
                     </tr>
                     {packsList.map((pack) => (
@@ -138,7 +143,7 @@ export const PacksList = () => {
                             <td className={s.tableCol}>{pack.user_name}</td>
                             <td className={s.tableCol}>{pack.updated}</td>
                             <td className={s.tableCol}>{pack.user_id}</td>
-                            {(checkedPrivate && success) && <ManagePacksButton _id={pack._id} deletePackFun={deletePackFun}/>}
+                            {(props.user_id) && <ManagePacksButton _id={pack._id} deletePackFun={deletePackFun}/>}
                             <td><NavLink to={`/cards-list/${pack._id}`} activeClassName={s.activeLink}>cards
                                 list</NavLink>
                             </td>
@@ -153,9 +158,16 @@ export const PacksList = () => {
                 />
             </div>
             <div className={finalBackgroundModal} onClick={() => setShowModal(false)}></div>
-            <div className={finalModal} onBlur={() => setShowModal(false)}>
+            <div className={finalModal} >
                 <button className={s.closeModalAdd} onClick={() => setShowModal(false)}>X</button>
-                <input className={s.inputModalAdd} type={'text'} value={title} onChange={changeTitle} />
+                <InputContainer
+                    placeholder={'Name pack'}
+                    changeValue={changeTitle}
+                    errorMessage={''}
+                    typeInput={'text'}
+                    value={title}
+                />
+                {/*<input className={s.inputModalAdd} type={'text'} value={title} onChange={changeTitle} />*/}
                 <button className={s.addModalAdd} onClick={addPackFun}>ADD</button>
             </div>
         </>
